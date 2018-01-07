bfb881984006a1bb42dd3a3a598c53e9b07cc7cd
Yuki <shipyuuki@yahoo.co.jp>
2018-01-07 21:35:49 +0900

    同じファイルシステム内で動くようにした

--- rsync.php
+++ rsync.php
@@ -7,15 +7,23 @@
 class rsync
 {
 
-    /**
-     * @var string 実際に実行するコマンド
-     */
-    private $command;
+    /* @var string $options オプション。aとかuとか */
+    private $options = "";
+    /* @var bool $is_delete --deleteオプションを付けるか */
+    private $is_delete = false;
+    /* @var string $from コピー元 */
+    private $from = "";
+    /* @var string $to コピー先 */
+    private $to = "";
 
+    /**
+     * rsync constructor.
+     * @throws RuntimeException
+     */
     public function __construct()
     {
         self::check_rsync();
-        $this->command = "rsync ";
+
     }
 
     /**
@@ -25,25 +33,82 @@ class rsync
      */
     private static function check_rsync()
     {
-        if (self::get_return_var("type rsync") !== 0) {
+        if (self::execute("type rsync")['return_var'] !== 0) {
             throw new RuntimeException("rsync command not found.");
         }
     }
 
     /**
-     * コマンドを実行し、終了ステータスを返す
+     * ディレクトリ自体をコピーする
+     * パスの末尾に強制で/を付ける
+     *
+     * @param string $from
+     */
+    public function from_dir_itself(string $from)
+    {
+        $from = rtrim($from, "/");
+        $this->from = $from . "/";
+    }
+
+    /**
+     * ディレクトリの中身またはファイル自体をコピーする
+     * パスの末尾の/を強制で取る
+     *
+     * @param string $from
+     */
+    public function from_file(string $from)
+    {
+        $from = rtrim($from, "/");
+        $this->from = $from;
+    }
+
+    /**
+     * オプションを設定する
+     *
+     * @param string $option
+     */
+    public function set_option(string $option)
+    {
+        $this->options .= $option;
+    }
+
+    /**
+     * --deleteオプションを有効にする
+     */
+    public function enable_delete()
+    {
+        $this->is_delete = true;
+    }
+
+    public function run()
+    {
+        $delete = $this->is_delete ? "--delete" : "";
+        $command = "rsync -" . $this->options . $delete . " " . $this->from . " " . $this->to;
+
+        $exec = self::execute($command);
+        if($exec['return_var'] !== 0){
+            throw new RuntimeException("failed to exec rsync.");
+        }
+    }
+
+    public function dry_run()
+    {
+        $this->set_option("n");
+        $this->run();
+    }
+
+    /**
+     * コマンドを実行し、実行結果と終了ステータスを返す
      *
      * @param string $command 実行するコマンド
-     * @return int 終了ステータス
+     * @return array('output'=>string[],'return_var'=>int) array('output'=>実行結果,'return_var'=>終了ステータス)
      */
-    private static function get_return_var(string $command): int
+    private static function execute(string $command): array
     {
         $output = array();
         $return_var = 1;
 
         exec($command, $output, $return_var);
-        return $return_var;
+        return array('output' => $output, 'return_var' => $return_var);
     }
-
-
 }
