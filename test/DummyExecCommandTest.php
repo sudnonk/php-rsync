4ffc835f02cc8f13d46ccc7f5e314abb39d9c7b7
sudnonk <sudnonk12@yahoo.co.jp>
2019-10-11 01:09:21 +0900

    テストを書いた

---
+++ test/DummyExecCommandTest.php
@@ -0,0 +1,20 @@
+<?php
+
+    namespace sudnonk\Rsync\Test;
+
+    use PHPUnit\Framework\TestCase;
+    use sudnonk\Rsync\DummyExecCommand;
+
+    class DummyExecCommandTest extends TestCase {
+        /**
+         * @test
+         */
+        public function ちゃんと指定された値を返す() {
+            $exec = new DummyExecCommand(0, true);
+            self::assertSame(0, $exec->execute(""));
+            self::assertSame(true, $exec->isRsyncEnabled());
+            $exec = new DummyExecCommand(1, false);
+            self::assertSame(1, $exec->execute(""));
+            self::assertSame(false, $exec->isRsyncEnabled());
+        }
+    }
