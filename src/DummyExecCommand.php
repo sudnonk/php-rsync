b2ac7c685dea89d8e498041f7ca15accd624c2b9
sudnonk <sudnonk12@yahoo.co.jp>
2019-10-10 22:45:38 +0900

    返り値を指定できるように

--- src/DummyExecCommand.php
+++ src/DummyExecCommand.php
@@ -1,12 +1,26 @@
 <?php
 
+    namespace sudnonk\Rsync;
 
     class DummyExecCommand implements ExecCommandInterface {
+        public $execute_return;
+        public $is_RsyncEnabled_return;
+
+        /**
+         * DummyExecCommand constructor.
+         * @param int  $execute_return         execute()が返す値
+         * @param bool $is_RsyncEnabled_return isRsyncEnabled()が返す値
+         */
+        public function __construct(int $execute_return, bool $is_RsyncEnabled_return) {
+            $this->execute_return = $execute_return;
+            $this->is_RsyncEnabled_return = $is_RsyncEnabled_return;
+        }
+
         public function execute(string $command): int {
-            return 0;
+            return $this->execute_return;
         }
 
         public function isRsyncEnabled(): bool {
-            return true;
+            return $this->is_RsyncEnabled_return;
         }
     }
